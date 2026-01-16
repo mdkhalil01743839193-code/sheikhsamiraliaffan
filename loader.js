4f6d772cf995da05089dedf151f8fd249fe660ef
Sheikh Samir Ali Affann <mdkhalil01743839193@gmail.com>
2026-01-16 13:30:51 +0600

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,41 +1,21 @@
-const lines = [
-  "ইসলাম জিন্দাবাদ",
-  "নারায়ে তাকবীর, আল্লাহু আকবার",
-  "আমি কেরানীগঞ্জ থেকে শেখ সামির আলি আফফান"
+const lines=[
+ "ইসলাম জিন্দাবাদ",
+ "নারায়ে তাকবীর, আল্লাহু আকবার",
+ "আমি কেরানীগঞ্জ থেকে শেখ সামির আলি আফফান"
 ];
 
-let lineIndex = 0;
-let charIndex = 0;
+let i=0,j=0;
+const t=document.getElementById("bootText");
+const l=document.getElementById("loader");
+const logo=document.getElementById("loaderLogo");
 
-const bootText = document.getElementById("bootText");
-const loader = document.getElementById("loader");
-const logo = document.getElementById("loaderLogo");
-
-function typeLine() {
-  if (lineIndex < lines.length) {
-    if (charIndex < lines[lineIndex].length) {
-      bootText.textContent += lines[lineIndex][charIndex];
-      charIndex++;
-      setTimeout(typeLine, 70);
-    } else {
-      bootText.textContent += "\n";
-      charIndex = 0;
-      lineIndex++;
-      setTimeout(typeLine, 600);
-    }
-  } else {
-    // logo reveal
-    logo.classList.add("show");
-
-    // loader fade out
-    setTimeout(() => {
-      loader.classList.add("fade");
-    }, 1200);
-
-    setTimeout(() => {
-      loader.style.display = "none";
-    }, 2300);
-  }
+function run(){
+ if(i<lines.length){
+  if(j<lines[i].length){t.textContent+=lines[i][j++];setTimeout(run,70)}
+  else{t.textContent+="\n";j=0;i++;setTimeout(run,600)}
+ }else{
+  logo.classList.add("show");
+  setTimeout(()=>l.style.display="none",2000)
+ }
 }
-
-typeLine();
+run();
